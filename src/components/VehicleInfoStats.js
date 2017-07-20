0e67975624027ced46735676a4ee0c5b74630c87
Jason Tuttle <jason@hearspotbark.com>
2017-07-20 15:39:40 -0400

    save state

---
+++ src/components/VehicleInfoStats.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class VehicleInfoStats extends Component {
+
+  render() {
+    return (
+      <div>VehicleInfoStats</div>
+    );
+  }
+
+}
